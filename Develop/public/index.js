f8a1a5c16d504d3d35b87765685353e2b70ff35b
Damian Isaac <damianisaacalexander@gmail.com>
2020-05-31 20:59:23 -0500

    added style/css loader

--- Develop/public/index.js
+++ Develop/public/index.js
@@ -1,3 +1,9 @@
+import "./assets/css/style.css"
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+
 let transactions = [];
 let myChart;
 
