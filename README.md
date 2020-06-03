f12227176be9db707c45b596e7abb6a313444e59
Damian Isaac <damianisaacalexander@gmail.com>
2020-06-03 16:47:15 -0500

    created env file, and changed mongoose connect to use envdb

---
+++ README.md
@@ -0,0 +1,26 @@
+# Budget Tracker PWA
+
+
+
+ ![Application Image](https://i.gyazo.com/57b3a109b5896dd4d26cb4a1c26edffa.jpg)
+
+## Application Features
+
+* Add/subract funds
+* Track expenses using a graph
+* Offline expense tracking
+* Database syncing from offline transactions
+
+## Built With
+
+* Jquery
+* MongoDB
+* Javascript 
+* Express
+* Webpack
+* CacheAPI
+* IndexedDB
+
+## Deployment
+
+https://damianisaacalexander.github.io/GetFed_Project/
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const compression = require("compression");
 
 const PORT = process.env.PORT || 3000;
 
@@ -9,18 +9,13 @@ const app = express();
 
 app.use(logger("dev"));
 
-app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
 
-// routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
